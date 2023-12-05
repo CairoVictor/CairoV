94f04d666b4e92c301c33dae821f29bb0f1777bb
Cairo <cairov271@gmail.com>
2023-12-05 08:19:48 -0300

    Primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+print("VAMOS CALCULAR ")
+num01 = int(input("PRIMEIRO NÚMERO : "))
+num02 = int(input("SEGUNDO NÚMERO: "))
+
+print("------------------- R E S U L T A D O -------------------")
+
+soma = num01 + num02
+print("RESULTADO DA SOMA: " , soma ) 
+sub = num01 - num02
+print("RESULTADO DA SUBTRAÇÃO: " , sub)
+mul = num01 * num02 
+print("RESULTADO DA MULTIPLICAÇÃO: " , mul)
+div = num01 // num02 
+print("RESULTADO DA DIVISÃO: " , div)
+resto = num01 % num02
+print("RESTO DA DIVISÃO: " , resto)
